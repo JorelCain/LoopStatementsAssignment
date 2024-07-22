ee43f683d717b8e86a4d29077e37c62544fb4aa0
JorelCain <jorelcain2@gmail.com>
2024-07-22 08:08:42 -0700

    Initial Commit

---
+++ loop_condition.py
@@ -0,0 +1,20 @@
+# Python Loop Statements - Assignment 2
+
+# Task 1 - Infinite loop with user input break statement
+first_car_make = True
+
+while first_car_make:
+    user = input("Can you guess my favorite creature that lives in the ocean?")
+    if user == "Shark":
+        break
+    if user == "shark":
+        break
+
+# Task 2 - Loop that terminates after 5 iterations and prints which iteration it is on
+
+guests = []
+
+while len(guests) < 5:
+    print("Welcome to my home, come in and grab a drink")
+    guests.append('person')
+    print("There are", len(guests), "people in my house right now")
---
+++ range_riddle.py
@@ -0,0 +1,8 @@
+# Python Loop Statements Assignment 1 - Range Riddle
+
+# Print the even days of the week
+
+days_of_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+for even_days in range(0, len(days_of_week), 2):
+    print(days_of_week[even_days])
